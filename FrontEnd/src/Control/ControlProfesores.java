970de6c0508fcd99340ae858fb7cc0682d0f4f09
alegambo <alegamboaba.17@gmail.com>
2019-02-25 23:29:41 +0100

    Parte del Front End (desktop)

---
+++ FrontEnd/src/Control/ControlProfesores.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import AccesoADatos.ServicioProfesor;
+import AccesoADatos.GlobalException;
+import AccesoADatos.NoDataException;
+import LogicaDeNegocio.Modelo;
+import LogicaDeNegocio.Profesor;
+import Vista.Vista;
+import java.util.LinkedList;
+/**
+ *
+ * @author Alejandro
+ */
+public class ControlProfesores {
+    ServicioProfesor domainModel;
+    Vista view;
+    Modelo model;
+     public ControlProfesores() throws GlobalException, NoDataException {
+        this(new Modelo(),new Vista());
+    }
+
+    public ControlProfesores(Modelo model,Vista view) throws GlobalException, NoDataException {
+        model.init();
+        this.domainModel= ServicioProfesor.getInstancia();
+        
+        this.view = view;
+        this.model = model;
+        iniciar();
+        view.setController(this);
+        view.setModel(model);
+    }
+    public void iniciar() throws GlobalException, NoDataException{
+        LinkedList lista = domainModel.listar();
+        
+        
+        this.model.setProfesores(domainModel.listar());
+    }
+    
+    public void buscar() throws GlobalException, NoDataException{
+        String nombre = view.nombreField.getText();
+        LinkedList lista = domainModel.listar();
+        LinkedList aux = model.getProfesores().buscar(nombre, lista);
+        model.setProfesores(aux);
+    }
+    
+    public void agregar() throws GlobalException, NoDataException{
+        //Profesor profesor_agregado = new Profesor(view.insertarId.getText(),view.insertarCedula.getText(),view.insertarEmail.getText());
+        //domainModel.insertar(profesor_agregado);
+        LinkedList<Profesor> rows = domainModel.listar();
+        if (rows.isEmpty()){
+            model.getErrores().put("nombreFld", "Ningun registro coincide");
+            model.setMensaje("NINGUN REGISTRO COINCIDE");
+        }
+        model.setProfesores(rows);
+    }
+
+    public void salir(){
+       
+    }
+}
---
+++ FrontEnd/src/frontend/FrontEnd.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frontend;
+import AccesoADatos.GlobalException;
+import AccesoADatos.NoDataException;
+import Control.ControlProfesores;
+/**
+ *
+ * @author Alejandro
+ */
+public class FrontEnd {
+
+  public static void main(String[] args) throws GlobalException, NoDataException {
+        // TODO code application logic here
+        ControlProfesores control = new ControlProfesores();
+            
+   
+        
+    } public static  final int  MODO_AGREGAR=0;
+    public static final int MODO_EDITAR=1;
+    
+}
